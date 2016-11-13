7c99588cc993047a875e77b1fae680220de08338
chatelao <olivier.chatelain@gmail.com>
2016-11-13 19:49:08 +0100

    Remove single mode

--- postgres-entry.sh
+++ postgres-entry.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 
-gosu postgres postgres --single -jE <<-EOL
+# gosu postgres postgres --single -jE <<-EOL
+gosu postgres psql <<-EOL
   CREATE USER "$OSM_USER";
 
   CREATE DATABASE "$OSM_DB";
   CREATE DATABASE "$OTO_DB";
+  CREATE DATABASE "contours";
 
   GRANT ALL ON DATABASE "$OSM_DB" TO "$OSM_USER";
   GRANT ALL ON DATABASE "$OTO_DB" TO "$OSM_USER";
+  GRANT ALL ON DATABASE "contours" TO "$OSM_USER";
 EOL
 
 # Postgis extension cannot be created in single user mode.
@@ -16,11 +19,11 @@ EOL
 # updating the DB, then shutting down the server so the
 # rest of the docker-postgres init scripts can finish.
 
-gosu postgres pg_ctl -w start
+# gosu postgres pg_ctl -w start
 gosu postgres psql "$OSM_DB" <<-EOL
   CREATE EXTENSION postgis;
   CREATE EXTENSION hstore;
-	CREATE EXTENSION dblink;
+  CREATE EXTENSION dblink;
   ALTER TABLE geometry_columns OWNER TO "$OSM_USER";
   ALTER TABLE spatial_ref_sys OWNER TO "$OSM_USER";
 EOL
@@ -28,6 +31,6 @@ EOL
 gosu postgres psql "$OTO_DB" <<-EOL
   CREATE EXTENSION postgis;
   CREATE EXTENSION hstore;
-	CREATE EXTENSION dblink;
+  CREATE EXTENSION dblink;
 EOL
-gosu postgres pg_ctl stop
+# gosu postgres pg_ctl stop
